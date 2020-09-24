2ec0b50ba3fc04ed4ec705872eb3efecc934168f
Jorge Sanes <jsanes@condorlabs.io>
2020-09-24 15:03:49 -0500

    improve cart behavior

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { ReactComponent as CartIcon } from './cart.svg';
+import { ReactComponent as DeleteIcon } from './delete.svg';
 
 function App(props) {
   const [cart, setCart] = useState([]);
   const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener('click', hideCart);
+
+    return () => {
+      document.removeEventListener('click', hideCart);
+    };
+  }, []);
+
+  function hideCart(event) {
+    if (event.target.className !== 'btn-cart') {
+      setShow(false);
+    }
+  }
 
   useEffect(() => {
     const currentItems = [...cart];
@@ -18,6 +34,14 @@ function App(props) {
     setShow(!show);
   }
 
+  function removeFromCart(itemName) {
+    const currentItems = [...cart];
+
+    const newItems = currentItems.filter((item) => item.name !== itemName);
+
+    setCart(newItems);
+  }
+
   function renderCartItems() {
     return cart.map((item, index) => (
       <div className="cart-item" key={`cart-item-${index}`}>
@@ -30,6 +54,12 @@ function App(props) {
             {item.price}
           </strong>
         </div>
+        <button
+          className="btn-remove"
+          onClick={() => removeFromCart(item.name)}
+        >
+          <DeleteIcon style={{ height: '25px', width: '25px' }} />
+        </button>
       </div>
     ));
   }
@@ -43,12 +73,23 @@ function App(props) {
     ).price;
   }
 
+  function preventClickThrough(event) {
+    event.stopPropagation();
+    event.nativeEvent.stopImmediatePropagation();
+  }
+
   return (
     <div className="shopping-cart">
-      <button onClick={handleCartVisibility}>{`Show cart ${
-        cart.length > 0 ? `(${cart.length})` : ''
-      }`}</button>
-      <div className={`cart-box ${show ? 'show' : ''}`}>
+      <button className="btn-cart" onClick={handleCartVisibility}>
+        <CartIcon
+          style={{ height: '30px', width: '30px', pointerEvents: 'none' }}
+        />
+        {cart.length > 0 ? <span>{cart.length}</span> : ''}
+      </button>
+      <div
+        className={`cart-box ${show ? 'show' : ''}`}
+        onClick={preventClickThrough}
+      >
         {cart.length === 0 ? (
           <h2>The cart is empty. Click on Add to Cart to start buying!</h2>
         ) : null}
