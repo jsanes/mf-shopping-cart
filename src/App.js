3846d50a304bae6d412f15152828cb2e496dda58
Jorge Sanes <jsanes@condorlabs.io>
2020-09-24 09:51:46 -0500

    improve the shopping cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [cart, setCart] = useState([]);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const currentItems = [...cart];
+    if (props.newItem.name) {
+      currentItems.push(props.newItem);
+      setCart(currentItems);
+    }
+  }, [props.newItem]);
+
+  function handleCartVisibility() {
+    setShow(!show);
+  }
+
+  function renderCartItems() {
+    return cart.map((item, index) => (
+      <div className="cart-item" key={`cart-item-${index}`}>
+        <img src={item.img} alt="" />
+        <div>
+          <h3>{item.name}</h3>
+          <p>{item.description}</p>
+          <strong>
+            {item.price ? '$' : ''}
+            {item.price}
+          </strong>
+        </div>
+      </div>
+    ));
+  }
+
+  function getTotalAmount() {
+    return cart.reduce(
+      (a, b) => ({
+        price: a.price + b.price,
+      }),
+      { price: 0 }
+    ).price;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="shopping-cart">
+      <button onClick={handleCartVisibility}>{`Show cart ${
+        cart.length > 0 ? `(${cart.length})` : ''
+      }`}</button>
+      <div className={`cart-box ${show ? 'show' : ''}`}>
+        {cart.length === 0 ? (
+          <h2>The cart is empty. Click on Add to Cart to start buying!</h2>
+        ) : null}
+        {renderCartItems()}
+        <strong>Total: ${getTotalAmount()}</strong>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.renderShoppingCart = (containerId, props) => {
+  ReactDOM.render(<App {...props} />, document.getElementById(containerId));
+  serviceWorker.unregister();
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+window.unmountShoppingCart = (containerId) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+if (!document.getElementById('ShoppingCart-container')) {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  serviceWorker.unregister();
+}
